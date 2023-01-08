0bce7ca3edfc8866b62f7c75822d2fb23163cecc
yun dabin <da8188@naver.com>
2023-01-08 15:24:31 +0900

    20230108

---
+++ Data_Structures/codingtest/백준2501번.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+result = []
+
+for i in range(1,N+1):
+    if (N % i) == 0:
+        result.append(i)
+        i = i + 1 
+        
+if len(result) < K:
+    print("0")
+
+else :
+    print(result[K-1])
