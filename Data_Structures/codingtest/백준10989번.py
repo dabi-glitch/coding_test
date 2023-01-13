0d5cfc53f4af2d16f57ddd51a4a7922481840a92
yun dabin <da8188@naver.com>
2023-01-13 23:06:25 +0900

    20230113

---
+++ Data_Structures/codingtest/백준10989번.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = [0] * 10001  #0부터 10000까지 이기 때문에 10001개
+
+for _ in range(N):
+  num = int(sys.stdin.readline()) 
+  arr[num] += 1 #num이 들어온 개수 count
+
+for i in range(10001):
+  if arr[i] != 0 :
+    for j in range(arr[i]):
+      print(i)
+
+
+
